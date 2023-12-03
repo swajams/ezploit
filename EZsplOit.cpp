77e553b5b59e7a7da272bc0f44eb046b7476da8d
swajams <saiswajabala@gmail.com>
2023-12-03 16:11:59 +0800

    first test

--- EZsplOit.cpp
+++ EZsplOit.cpp
@@ -2,8 +2,93 @@
 using namespace std;
 
 
-int main(){
+
+int menu() {
+    int choice;
+    bool validInput = false;
+
+    do {
+        std::cout << "What would you like to do?" << std::endl;
+        std::cout << "1. Scan" << std::endl;
+        std::cout << "2. Targets" << std::endl;
+        std::cout << "3. Attacks" << std::endl;
+
+        string input;
+        char option;
+        std::cin >> input;
+        if(input.length() == 1){
+             option = input[0];
+        }
+        else{
+            option = 0;
+        }
+
+        switch (option) {
+            case '1':
+                choice = 1;
+                validInput = true;
+                break;
+            case '2':
+                choice = 2;
+                validInput = true;
+                break;
+            case '3':
+                choice = 3;
+                validInput = true;
+                break;
+            default:
+                std::cout << "INVALID INPUT! Please enter a valid option." << std::endl;
+        }
+    } while (!validInput);
+
+    return choice;
+
+}
+
+int scan(){
     
-    cout << "Hello World" << endl;
+    cout << "Scanning..." << endl;
+    const char* ScanCommand ="ifconfig";
+    int result = system(ScanCommand);
+}
+
+
+
+int main() {
+  cout << R"(
+       __             _,-^~^"-.
+     _// )      _,-^~`         `.
+   ." ( /`"-,-"`                 ;
+  / @            EZsplOit         ;
+ /           ,             ,-"     ;
+(,__.--.      \           /        ;
+       /`-.\   |          |        `._________
+   _.-'_/`  )  )--...,,,___\     \-----------,)
+ ((("~` _.-'.-'           __`-.   )         //
+       ((("`             (((---~"`         //
+                                          ((________________
+                                          `----^^^^^^^^''''
+)" <<endl;
+int option;
+option = menu();
+
+if(option == 1 ){scan();}
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
